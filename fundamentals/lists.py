0b314e3bf40fcf43e6aeeaf4137f7723af422378
Kari Smith <kariasmith@hotmail.com>
2023-08-15 02:19:46 -0600

    Update lists.py

--- fundamentals/lists.py
+++ fundamentals/lists.py
@@ -1,1 +1,29 @@
 
+fruits = ['apple', 'banana', 'orange', 'strawberry']
+vegetables = ['lettuce', 'cucumber', 'carrots']
+fruits_and_vegetables = fruits + vegetables
+print(fruits_and_vegetables)
+salad = 3 * vegetables
+print(salad)
+
+
+drawers = ["documents", "envelopes", "pens"]
+    
+# access the drawer with index of 0 and print value
+print(drawers[0])  #prints documents
+# access the drawer with index of 1 and print value
+print(drawers[1]) #prints envelopes
+# access the drawer with index of 2 and print value
+print(drawers[2]) #prints pens
+    
+# replace "documents" with "tchotchkes"
+drawers[0] = "tchotchkes"
+print(drawers) # prints ["tchotchkes", "envelopes", "pens"]
+    
+# stores the value "tchotchkes" in a temporary variable.
+top_contents = drawers[0]
+    
+# Replaces the value at index 1
+# with whatever value is stored at index 0
+drawers[1] = drawers[0]
+print(drawers) # prints ["tchotchkes", "tchotchkes", "pens"]
