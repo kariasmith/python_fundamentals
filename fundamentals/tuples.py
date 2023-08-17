33bb4a51d7ff977f7a9f197bf0912798f77f5514
Kari Smith <kariasmith@hotmail.com>
2023-08-16 21:07:52 -0600

    Update tuples.py

--- fundamentals/tuples.py
+++ fundamentals/tuples.py
@@ -1,1 +1,23 @@
+'''A Tuple is a container for a fixed sequence of data objects.
+Tuples are sequences, just like lists. 
+The only difference is that tuples can't be changed -- that is, tuples are immutable. 
+Also, while lists are defined using square brackets, tuples use parentheses. 
+Creating a tuple is as simple as declaring different comma-separated values. 
+Optionally you can put these values between parentheses.'''
 
+tuple_data = ('physics', 'chemistry', 1997, 2000)
+tuple_num = (1, 2, 3, 4, 5 )
+tuple_letters = "a", "b", "c", "d"
+
+'''A tuple can be used to group any number of items into a single compound value. 
+Syntactically, a tuple is a comma-separated sequence of values. 
+Although it is not necessary, it is conventional to enclose tuples in parentheses.'''
+
+dog = ("Canis Familiaris", "dog", "carnivore", 12)
+
+'''So far, you've probably built enough pattern-recognition in coding to intuitively know that parentheses, 
+(), almost always mean you're dealing with a function -- a function call, the beginning of a list of 
+arguments for a function, or the definition of a function. But watch out, occasionally you may be dealing 
+with a tuple!'''
+
+# https://docs.python.org/3/tutorial/datastructures.html#tuples-and-sequences
