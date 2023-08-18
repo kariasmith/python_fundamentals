006450870b488a01c021c9c19ca51e89ff410edc
Kari Smith <kariasmith@hotmail.com>
2023-08-17 20:21:03 -0600

    Create conditionals.py

---
+++ fundamentals/conditionals.py
@@ -0,0 +1,3 @@
+'''Conditional statements allow us to run certain lines of code depending on whether certain 
+conditions are met.  These statements control the flow of how our code is executed by the interpreter.  
+In Python, the keywords for conditional statements are if, elif, and else. Here are some examples:'''
