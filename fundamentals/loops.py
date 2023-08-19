909f9de30e15e66e38b0a38b37a774ef51c5c567
Kari Smith <kariasmith@hotmail.com>
2023-08-18 19:49:33 -0600

    Update loops.py

--- fundamentals/loops.py
+++ fundamentals/loops.py
@@ -1,1 +1,10 @@
+# Loops
+'''We can use Python's range function in conjunction with python for loops to repeat the same code many 
+times with a number, usually called i that represents an integer that changes. This is similar to for 
+loops in other languages, but the syntax is a bit different. The range() function can have between 
+1 and 3 arguments and creates a sequence of integers called a range object. We primarily use this for 
+creating loops that have a defined starting point and a stopping point for a sequence of integers.
+
+I like to think of this built in range() function as having the three S’es or “start, stop, step.” Easy to remember
+'''
 
