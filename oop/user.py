4cf27efbf29ee71f7563c7c5fa328151257d0636
Kari Smith <kariasmith@hotmail.com>
2023-08-27 11:38:40 -0600

    Add files via upload

---
+++ oop/user.py
@@ -0,0 +1,32 @@
+class User:		
+    def __init__(self):
+        self.first_name = "Ada"
+        self.last_name = "Lovelace"
+        self.age = 42
+
+user_ada = User()
+print(user_ada.first_name)
+
+# Create another user called user_2!
+
+# Print user_ada's last name.
+# Print user_2's last name. (Yes, they should be the same)
+
+# Run the code, pause, go back and step through one line at a time
+# What do you notice about the order it runs in?
+# Write down any other questions you have.
+
+# Sensei Exercise: try just printing the variable, user_ada.
+#   What prints to the terminal?
+
+
+'''
+what does '->' do?
+
+This is optional and for specifying a return type. You don't have to do this in Python but it's useful if you're looking quickly through your code and for some other reasons too\
+
+-> None means there is no return or it returns None
+-> int would mean it returns an integer
+'''
+
+# ____ Mifflin duunder}
