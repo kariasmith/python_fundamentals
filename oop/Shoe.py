78db82fd60a706166dd60e4122ea12073f9703a2
Kari Smith <kariasmith@hotmail.com>
2023-08-28 21:14:36 -0600

    Add files via upload

---
+++ oop/Shoe.py
@@ -0,0 +1,19 @@
+class Shoe:
+    # now our method has 4 parameters (including self)!
+    def __init__(self, brand, shoe_type, price):
+    	# we assign them accordingly
+        self.brand = brand
+        self.type = shoe_type
+        self.price = price
+        # the status is set to True by default
+        self.in_stock = True
+
+skater_shoe = Shoe("Vans", "Low-top Trainers", 59.99)
+dress_shoe = Shoe("Jack & Jill Bootery", "Ballet Flats", 29.99)
+print(skater_shoe.type)	# output: Low-top Trainers
+print(dress_shoe.type)	# output: Ballet Flats
+
+# Ninja Challenges!
+# Open this code on the Trace website to get a better view of all the variables and their attributes.
+# Make a new instance of a shoe
+# Update the in_stock attribute to False
